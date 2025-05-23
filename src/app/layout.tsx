fe407faf36410f79863bdd84c7fe1bcda38cfd4c
ZhiXuan Zhong <dooi921@gmail.com>
2025-05-23 09:19:48 +0800

    feat: UI Library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css';
+import { LucideKanban } from 'lucide-react';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
 import { homePath, ticketsPath } from '@/paths';
 
 const geistSans = Geist({
@@ -36,12 +38,14 @@ export default function RootLayout({
           "
         >
           <div>
-            <Link href={homePath()} className="text-lg font-bold">
-              Home
+            {/* [筆記點A] */}
+            <Link href={homePath()} className={buttonVariants({ variant: 'ghost' })}>
+              <LucideKanban />
+              <h1 className=" text-lg font-semibold">TicketBounty</h1>
             </Link>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-sm underline">
+            <Link href={ticketsPath()} className={buttonVariants({ variant: 'default' })}>
               Tickets
             </Link>
           </div>
@@ -61,3 +65,27 @@ export default function RootLayout({
     </html>
   );
 }
+
+/*
+ * 開發學習紀錄:
+ *
+ * == 2025-05-20 ==
+ * - [筆記點A] 讓 Link 擁有 Button 的樣式，這是邊緣案例不太常用。以下兩種做法可以達成
+ *   1. 使用 asChild 是為了讓 DOM 結構符合語意，以 Button 的樣式傳至 Link 上，所以最終將只有 <a></a> 被渲染在頁面上，不會看到 Button 的元素
+ *   - 範例：
+ *     ```tsx
+ *     <Button asChild variant="outline">
+ *       <Link href={homePath()}>Home</Link>
+ *     </Button>
+ *     ```
+ *
+ *   2. 在 className 上使用 buttonVariants 然後傳入需要的 variant object
+ *   - 範例：
+ *     ```tsx
+ *     <Link href={homePath()} className={buttonVariants({ variant: 'outline' })}>
+ *       Home
+ *     </Link>
+ *     ```
+ *
+ * ===
+ */
